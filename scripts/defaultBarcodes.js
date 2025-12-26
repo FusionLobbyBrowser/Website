9179788f9b65296c1d80fcf607f44570e7f7c777
HAHOOS <hahoos@hahoos.dev>
2025-12-26 11:01:59 +0100

    chore: move default barcodes to a separate file
    Signed-off-by: HAHOOS <hahoos@hahoos.dev>

---
+++ scripts/defaultBarcodes.js
@@ -0,0 +1,206 @@
+const mappedBarcodes = [
+  // Avatars
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Avatar.Heavy",
+    name: "Heavy",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Avatar.Fast",
+    name: "Fast",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Avatar.CharFurv4GB",
+    name: "Short",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Avatar.CharTallv4",
+    name: "Tall",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Avatar.Strong",
+    name: "Strong",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Avatar.Anime",
+    name: "Light",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Avatar.CharJimmy",
+    name: "Jay",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Avatar.FordBW",
+    name: "Ford",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Avatar.CharFord",
+    name: "Ford",
+  },
+  {
+    barcode: "SLZ.BONELAB.Core.Avatar.PeasantFemaleA",
+    name: "Peasant",
+  },
+  {
+    barcode: "c3534c5a-10bf-48e9-beca-4ca850656173",
+    name: "Peasant",
+  },
+  {
+    barcode: "c3534c5a-2236-4ce5-9385-34a850656173",
+    name: "Peasant",
+  },
+  {
+    barcode: "c3534c5a-87a3-48b2-87cd-f0a850656173",
+    name: "Peasant",
+  },
+  {
+    barcode: "c3534c5a-f12c-44ef-b953-b8a850656173",
+    name: "Peasant",
+  },
+  {
+    barcode: "c3534c5a-3763-4ddf-bd86-6ca850656173",
+    name: "Peasant",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Avatar.Nullbody",
+    name: "Nullbody",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Avatar.Charskeleton",
+    name: "Skeleton",
+  },
+  {
+    barcode: "c3534c5a-d388-4945-b4ff-9c7a53656375",
+    name: "Security Guard",
+  },
+  {
+    barcode: "c3534c5a-94b2-40a4-912a-24a8506f6c79",
+    name: "PolyBlank",
+  },
+  // Maps
+  {
+    barcode: "c2534c5a-80e1-4a29-93ca-f3254d656e75",
+    name: "Main Menu",
+  },
+  {
+    barcode: "c2534c5a-4197-4879-8cd3-4a695363656e",
+    name: "Descent",
+  },
+  {
+    barcode: "c2534c5a-6b79-40ec-8e98-e58c5363656e",
+    name: "BONELAB Hub",
+  },
+  {
+    barcode: "c2534c5a-56a6-40ab-a8ce-23074c657665",
+    name: "LongRun",
+  },
+  {
+    barcode: "c2534c5a-54df-470b-baaf-741f4c657665",
+    name: "Mine Dive",
+  },
+  {
+    barcode: "c2534c5a-7601-4443-bdfe-7f235363656e",
+    name: "Big Anomaly",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Level.LevelStreetPunch",
+    name: "Street Puncher",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Level.SprintBridge04",
+    name: "Sprint Bridge",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Level.SceneMagmaGate",
+    name: "Magma Gate",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Level.MoonBase",
+    name: "Moon Base",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Level.LevelKartRace",
+    name: "Monogon Motorway",
+  },
+  {
+    barcode: "c2534c5a-c056-4883-ac79-e051426f6964",
+    name: "Pillar Climb",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Level.LevelBigAnomalyB",
+    name: "Big Anomaly B",
+  },
+  {
+    barcode: "c2534c5a-db71-49cf-b694-24584c657665",
+    name: "Ascent",
+  },
+  {
+    barcode: "fa534c5a868247138f50c62e424c4144.Level.VoidG114",
+    name: "VoidG114",
+  },
+  {
+    barcode: "c2534c5a-61b3-4f97-9059-79155363656e",
+    name: "Baseline",
+  },
+  {
+    barcode: "c2534c5a-2c4c-4b44-b076-203b5363656e",
+    name: "Tuscany",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Level.LevelMuseumBasement",
+    name: "Museum Basement",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Level.LevelHalfwayPark",
+    name: "Halfway Park",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Level.LevelGunRange",
+    name: "Gun Range",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Level.LevelHoloChamber",
+    name: "HoloChamber",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Level.LevelKartBowling",
+    name: "Big Bone Bowling",
+  },
+  {
+    barcode: "SLZ.BONELAB.Content.Level.LevelMirror",
+    name: "Mirror",
+  },
+  {
+    barcode: "c2534c5a-4f3b-480e-ad2f-69175363656e",
+    name: "Neon District Tac Trial",
+  },
+  {
+    barcode: "c2534c5a-de61-4df9-8f6c-416954726547",
+    name: "Drop Pit",
+  },
+  {
+    barcode: "c2534c5a-c180-40e0-b2b7-325c5363656e",
+    name: "Tunnel Tipper",
+  },
+  {
+    barcode: "fa534c5a868247138f50c62e424c4144.Level.LevelArenaMin",
+    name: "Fantasy Arena",
+  },
+  {
+    barcode: "c2534c5a-162f-4661-a04d-975d5363656e",
+    name: "Container Yard",
+  },
+  {
+    barcode: "c2534c5a-5c2f-4eef-a851-66214c657665",
+    name: "Dungeon Warrior",
+  },
+  {
+    barcode: "c2534c5a-c6ac-48b4-9c5f-b5cd5363656e",
+    name: "Rooftops",
+  },
+  {
+    barcode: "fa534c5a83ee4ec6bd641fec424c4142.Level.SceneparkourDistrictLogic",
+    name: "Neon District Parkour",
+  },
+];
+
+export default mappedBarcodes;
