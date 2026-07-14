ee7e7115ef85979331abe26820306082d849f037
hahoos <hahoos@hahoos.dev>
2026-07-14 14:11:12 +0200

    fix: wrong property
    Signed-off-by: hahoos <hahoos@hahoos.dev>

--- scripts/main.js
+++ scripts/main.js
@@ -522,7 +522,7 @@ async function createLobby(lobby, signal, hidden) {
   }
   lobbyElem.setAttribute("platform", lobby.lobbyPlatform);
   const icon = lobbyElem.getElementsByClassName("platformIcon")[0];
-  icon.classes = "";
+  icon.className = "";
   icon.classList.add("platformIcon");
   if (lobby.lobbyPlatform == "Steam") {
     icon.classList.add("fa-brands");
