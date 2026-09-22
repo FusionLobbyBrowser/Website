ca0596b6d9e0ae613f716ef9be09eaead505a8e0
HAHOOS <hahoos@hahoos.dev>
2026-09-22 17:17:01 +0200

    chore(workflow): dont check formatting
    Signed-off-by: HAHOOS <hahoos@hahoos.dev>

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,14 @@
 const { defineConfig } = require("eslint/config");
 const eslintConfigESLintBase = require("eslint-config-eslint/base");
 const eslintConfigESLintCJS = require("eslint-config-eslint/cjs");
-const eslintConfigESLintFormatting = require("eslint-config-eslint/formatting");
 
 module.exports = defineConfig([
   {
     files: ["scripts/*.js"],
-    extends: [eslintConfigESLintBase, eslintConfigESLintFormatting],
+    extends: [eslintConfigESLintBase],
   },
   {
     files: ["eslint.config.js", ".eleventy.js", "tools/*.js"],
-    extends: [eslintConfigESLintCJS, eslintConfigESLintFormatting],
+    extends: [eslintConfigESLintCJS],
   },
 ]);
